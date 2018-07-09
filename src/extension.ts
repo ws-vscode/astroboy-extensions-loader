937ccb44d5e3bc0ef45ed73845737b85a10b7825
Big Mogician <sunaoran@youzan.com>
2018-07-09 17:08:01 +0800

    fix launchs

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+console.log("this is extensions");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -26,4 +27,5 @@ export function activate(context: vscode.ExtensionContext) {
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+    console.log("extension syop.");
 }
